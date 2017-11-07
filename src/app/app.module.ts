280fcf761abf53aecfde883cef5c963891d789e2
VCalosch <carlos.villanueva@alumnos.uneatlantico.es>
2017-11-07 10:40:56 +0100

    [Actualizacion-Front]
    Se sigue modificando la Home y se ha introducido las rutas para login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
-
-
+import { LoginComponent } from './components/login/login.component';
 const app_routes: Routes = [
 { path: 'navbar', component: NavbarComponent},
 { path: 'footer', component: FooterComponent},
 { path: 'home', component: HomeComponent},
+{ path: 'login', component: LoginComponent},
 { path: '**', pathMatch: 'full', redirectTo: 'navbar' }
 ];
 
